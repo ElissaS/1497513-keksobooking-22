e58d7268a42625a3c9e45f29a0c2afd8fe3ce6da
Alesyacd <sobolevskaya1408@gmail.com>
2021-03-12 23:04:57 +0300

    финал 5.1

--- js/draw-suggestion.js
+++ js/draw-suggestion.js
@@ -6,55 +6,59 @@ const drawSuggestion = (suggestion) => {
 
   const temp = document.querySelector('#card');
   const content = temp.content.cloneNode(true);
+  console.log(suggestion);
 
   content.querySelector('.popup__title').innerText = suggestion.offer.title;
   content.querySelector('.popup__text--address').innerText = suggestion.offer.address;
   content.querySelector('.popup__text--price').innerText = suggestion.offer.price;
-  content.querySelector('.popup__type').innerText = types[suggestion.offer.type];
+
   const types = {
     flat: 'Квартира',
     bungalow: 'Бунгало',
     house: 'Дом',
     palace: 'Дворец',
   }
+  content.querySelector('.popup__type').innerText = types[suggestion.offer.type];
+
 
   content.querySelector('.popup__text--capacity').innerText = `${suggestion.offer.rooms} комнаты для ${suggestion.offer.guests} гостей`;
   content.querySelector('.popup__text--time').innerText = `Заезд после ${suggestion.offer.checkin}, выезд до ${suggestion.offer.checkout}`;
 
 
   const featuresPopup = content.querySelector('.popup__features')
-  const featuresContent = content.querySelector('.popup__features').innerText = suggestion.offer.features;
-
 
   const renderFeaturesList = (features, parentBlock) => {
+    parentBlock.innerHTML = '';
     features.forEach((feature) => {
       const newFeature = document.createElement('li');
-      newFeature.classList.add('popup__feature', `popup__feature--${features[feature]}`);
+      newFeature.classList.add('popup__feature', `popup__feature--${feature}`);
       parentBlock.appendChild(newFeature);
     });
   };
-  renderFeaturesList(featuresContent, featuresPopup);
+  renderFeaturesList(suggestion.offer.features, featuresPopup);
 
   content.querySelector('.popup__description').innerText = suggestion.offer.description;
 
-  const photosTemp = content.querySelectorAll('.popup__photos');
+  const photosTemp = content.querySelector('.popup__photos');
   const renderAdPhotos = (photos, photoGallery) => {
-    photos.forEach ((photo) => {
+    photoGallery.innerHTML = '';
+    photos.forEach((photo) => {
       const newPhoto = document.createElement('img');
-      newPhoto.src = photos[photo];
+      newPhoto.src = photo;
       newPhoto.width = '45';
       newPhoto.height = '40';
       newPhoto.alt = 'Фотография жилья';
+      newPhoto.className = 'popup__photo'
       photoGallery.appendChild(newPhoto);
     });
   };
-  // массив для перебора фоток надо импортировать из data или из suggestions...не пойму)
-  renderAdPhotos(photos, photosTemp);
+
+  renderAdPhotos(suggestion.offer.photos, photosTemp);
 
   content.querySelector('.popup__avatar').src = suggestion.author.avatar;
   document.querySelector('#map-canvas').appendChild(content);
 }
 
-drawSuggestion();
+drawSuggestion(suggestions[0]);
 
 export { drawSuggestion };
