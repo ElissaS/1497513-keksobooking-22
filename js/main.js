b08b218e3e15c8c88a09881cadacd35ca953a796
Alesyacd <sobolevskaya1408@gmail.com>
2021-03-14 12:38:11 +0300

    точно финал 5.2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 import { suggestions } from './data.js';
 import { drawSuggestion } from './draw-suggestion.js';
-import { priceChangeHandler, checkinChangeHandler } from './field-handler.js';
+import { priceChangeHandler, checkinChangeHandler, checkoutChangeHandler } from './field-handler.js';
