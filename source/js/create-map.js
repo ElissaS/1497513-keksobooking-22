a39f8648c5cfee4244467acef39a9e10e1d8d4f3
Alesyacd <sobolevskaya1408@gmail.com>
2021-03-28 00:29:01 +0300

    финал

--- source/js/create-map.js
+++ source/js/create-map.js
@@ -26,7 +26,7 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: '/img/main-pin.svg',
+  iconUrl: '../img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
@@ -54,7 +54,7 @@ const createMarkers = (array) => {
     const { lat, lng } = item.location;
 
     L.icon({
-      iconUrl: '/img/pin.svg',
+      iconUrl: '../img/pin.svg',
       iconSize: [40, 40],
       iconAnchor: [20, 40],
     });
